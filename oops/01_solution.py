31abdf5f50a63438223b705cd97029aa9edfc1ae
Prince Pal <princepal9120@gmail.com>
2024-10-15 22:58:14 +0530

    python oops

---
+++ oops/01_solution.py
@@ -0,0 +1,14 @@
+#basic class and object
+# problem-> create a car class with attributes like brand and model. then create a instance of this class.
+
+class Car:
+    def __inti__ (self,brand,model): 
+        self.brand=brand
+        self.model=model
+
+
+my_car=Car()
+print(my_car)
+
+
+     
